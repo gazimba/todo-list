c301f0016bb66fd050a47be69f2c6ef73698953c
gazimba <gabrielb3r@gmail.com>
2025-02-18 10:19:44 -0300

    types e contexts

---
+++ src/contexts/reducers/todoReducer.ts
@@ -0,0 +1,29 @@
+import { TodoState, TodoActions } from "../../@types/TodoReducer"
+
+export const initialState: TodoState = []
+
+export const todoReducer = (state: TodoState, action: TodoActions) => {
+    switch (action.type) {
+        case "ADD":
+            const { payload } = action
+            return !Array.isArray(payload) ? [...state, payload] : payload
+
+        case "CHANGE":
+            const changedTodos = state.map((item, key) => {
+                if (key === action.payload.index) {
+                    item.title = action.payload.title ?? item.title
+                    item.isDone = action.payload.isDone ?? item.isDone
+                }
+
+                return item
+            })
+
+            return changedTodos
+
+        case "DELETE":
+            return state.filter((_, key) => key != action.payload.index)
+            
+        default:
+            return state
+    }
+}
